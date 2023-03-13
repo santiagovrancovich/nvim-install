75f5143d0cb9b8e9c1d6e1ba9b22a4ea04e42968
Santiago Vrancovich <103597690+santiagovrancovich@users.noreply.github.com>
2023-03-13 19:00:36 -0300

    Updated DAP mappings

--- custom/core/mappings.lua
+++ custom/core/mappings.lua
@@ -5,11 +5,11 @@ M.general = {
   n = {
      ["<C-a>"] = {"ggVG", "Select all text"},
      ["<F5>"] = {"<Cmd>lua require'dap'.continue()<CR>", "Run DAP client"},
-     ["<F6>"] = {"<Cmd>lua require'dap'.terminate()<CR>", "Terminate DAP client"},
-     ["<F7>"] = {"<Cmd>lua require'dap'.restart()<CR>", "Restart current DAP session"},
-     ["<F10>"] = {"<Cmd>lua require'dap'.step_over()<CR>", "DAP Step over"},
-     ["<F11>"] = {"<Cmd>lua require'dap'.step_into()<CR>", "DAP Step into"},
-     ["<F12>"] = {"<Cmd>lua require'dap'.step_out()<CR>", "DAP Step out"},
+     ["<F6>"] = {"<Cmd>lua require'dap'.step_into()<CR>", "DAP Step into"},
+     ["<F7>"] = {"<Cmd>lua require'dap'.step_over()<CR>", "DAP Step over"},
+     ["<F8>"] = {"<Cmd>lua require'dap'.step_out()<CR>", "DAP Step out"},
+     ["<F9>"] = {"<Cmd>lua require'dap'.restart()<CR>", "Restart current DAP session"},
+     ["<F10>"] = {"<Cmd>lua require'dap'.terminate()<CR>", "Terminate DAP client"},
      ["<C-D>"] = {"<Cmd>lua require'dapui'.toggle()<CR>", "Toggle DAP UI"},
      ["<C-b>"] = {"<Cmd>lua require'dap'.toggle_breakpoint()<CR>", "Breakpoint"},
   }
